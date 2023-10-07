96edec4c951cce21b8f6ae0abb823d6e5416a701
Prof Prof <domingo.joseph@outlook.ph>
2023-10-07 12:55:10 +0800

    Add project files.

---
+++ WebDev2/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebDev2.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
